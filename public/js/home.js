5906b4e94420bb4e61e7d792852fcd6964300a6e
AliahG97 <143744575+AliahG97@users.noreply.github.com>
2024-03-23 18:03:30 -0400

    add comment button fetch added

--- public/js/home.js
+++ public/js/home.js
@@ -1,11 +1,18 @@
-console.log('hi')
 const addCommentBtns = document.getElementsByClassName('addComment');
 console.log(addCommentBtns);
 for(let i =0; i < addCommentBtns.length; i++){
     addCommentBtns[i].addEventListener('click', function(){
+        const postId = addCommentBtns[i].dataset.postId;
+
+        fetch(`/comment/${postId}`)
+            .then(response => response.json())
+            .then(comments => {
+                console.log(comments);
+            })
+            .catch(error => console.error('Error fetching comments:', error));
       
 
-        // fetch()
+        
     })
     
 }
