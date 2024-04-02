2d5a930a34d210a8c8f0ba91dac7524df31941aa
AliahG97 <143744575+AliahG97@users.noreply.github.com>
2024-04-02 12:32:46 -0400

    initial commit

--- controllers/api/blog-routes.js
+++ controllers/api/blog-routes.js
@@ -17,7 +17,7 @@ router.get('/comment/:id', async (req, res) => {
 });
 
 // Put (update posts)
-router.put('/', withAuth, async (req, res) => {
+router.put('/:id', withAuth, async (req, res) => {
     try {
         console.log("AAAAAAAAAAAA")
         console.log(req.body)
@@ -25,8 +25,14 @@ router.put('/', withAuth, async (req, res) => {
         //we need to use update method on the BlogPost
     
         // TODO: fix this! checkout the CRUD activities in Sequalized Module 13
-        let updateBlog = await BlogPost.update({id: req.body.id}, req.body);
-       
+        const updateBlog = await BlogPost.findByPk(req.params.id);
+        if (!updateBlog){
+            return res(404).json({ error: "Blog post not found"});
+        }
+       await updateBlog.update(req.body);
+       res.status(200).json(updateBlog);
+      
+
     } catch (err) {
         console.error(err);
         res.status(500).json(err);
@@ -35,12 +41,13 @@ router.put('/', withAuth, async (req, res) => {
 
 router.delete('/:id', withAuth, async (req, res) => {
     try {
-        const blogPostData = await Post.findByPk(req.params.id);
+        const blogPostData = await BlogPost.findByPk(req.params.id);
 
         if (blogPostData) {
 
             await blogPostData.destroy();
-            res.redirect('/');
+            res.status(200).json({ message:'Post deleted successfully' });
+         
         } else {
             res.status(404).json({ message: 'Post not found'});
         }
